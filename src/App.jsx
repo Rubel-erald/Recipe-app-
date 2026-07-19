08ffa24058aeec8079a95b6f1eca11e5db74b4d0
Rubel erald <your-rubelerald.j@gmail.com>
2026-07-19 10:39:55 +0530

    pages rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import RecipeDetails from "./pages/RecipeDetails";
+import HomePage from "./pages/homePage";
+import RecipeDetails from "./pages/recipeDetails";
 
 function App() {
   return (
